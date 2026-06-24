d06b59a01ac4d0406c248f2360dd667d92bc09ec
pappas1paul-cpu <pappas1.paul@gmail.com>
2026-06-24 22:46:44 +0300

    CH:2 / LS:1

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from textnode import TextNode
+print("hello world")
+def main():
+    
+    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    
+    # Printing the object
+    print(node)
+
+
+if __name__ == "__main__":
+    main()
+   
